ff7b72743b7730c36bfbd5026feb93b2bdbeaf99
DFAFF <dafa.alfarisi39@gmail.com>
2024-11-25 15:06:35 +0700

    25 Nopember 2024

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ Route::get('dosen', 'App\Http\Controllers\DosenController@index');
 Route::get('blog2', 'App\Http\Controllers\DosenController@blog');
 Route::get('biodata', 'App\Http\Controllers\DosenController@biodata');
 
-Route::get('/pegawai/{nama}', 'App\Http\Controllers\PegawaiController@index');
+//Route::get('/pegawai/{nama}', 'App\Http\Controllers\PegawaiController@index');
 
 
 Route::get('/formulir', 'App\Http\Controllers\PegawaiController@formulir');
@@ -48,3 +48,12 @@ Route::get('/home1/responsive', 'App\Http\Controllers\EtsController@responsive')
 Route::get('/home1/form', 'App\Http\Controllers\EtsController@form');
 Route::get('/home1/linktree', 'App\Http\Controllers\EtsController@linktree');
 Route::get('/home1/testimoni', 'App\Http\Controllers\EtsController@testimoni');
+
+//route CRUD
+Route::get('/pegawaiDB','App\Http\Controllers\PegawaiDBController@index');
+Route::get('/pegawaiDB/tambah','App\Http\Controllers\PegawaiDBController@tambah');
+Route::post('/pegawaiDB/store','App\Http\Controllers\PegawaiDBController@store');
+Route::get('/pegawaiDB/hapus/{id}','App\Http\Controllers\PegawaiDBController@hapus');
+Route::get('/pegawaiDB/edit/{id}','App\Http\Controllers\PegawaiDBController@edit');
+Route::post('/pegawaiDB/update','App\Http\Controllers\PegawaiDBController@update');
+Route::get('/pegawaiDB/cari','App\Http\Controllers\PegawaiDBController@cari');
